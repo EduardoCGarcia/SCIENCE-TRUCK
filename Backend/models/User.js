8200b8f264c614d2a712d6f9d640e19da019760a
EduardoCGarcia <echavezg004@gmail.com>
2022-06-02 09:27:48 -0500

    modelo User y dependencias de encriptamiento y de avatar

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,15 +1,38 @@
 const mongoose = require('mongoose');
+const bycript = require('bcrypt-nodejs');
+const crypto = require('crypto');
+
+
 const UserSchema = mongoose.Schema({
-    userName:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    }
-    
+    email: { type: String, unique: true, lowercase: true, required: true },
+    displayName: String,
+    avatar: String,
+    password: { type: String, delect: false, required: true },
+    singupDate: { type: Date, default: Date.now() },
+    lastLogin: Date
 });
 
-module.exports = mongoose.model('User',UserSchema);
+UserSchema.pre('save', (next) => {
+    let user = this;
+    if (!user.isModified('password')) return next()
+
+    bycript.genSalt(10, (err, salt) =>{
+        if(err) return next(err);
+        bycript.hash(user.password,salt,null,(err,hash) =>{
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        })
+    })
+})
+
+UserSchema.methods.gravatar = function ( ) {  
+    if(!this.email) return `https://gravatar.com/avatar/?s=200&d=retro`
+
+    const md5 = crypto.createHash(md5).update(this.email).digest('hex');
+    return `https://gravar.com/avatar/${md5}?s=200&d=retro`;
+ }
+
+module.exports = mongoose.model('User', UserSchema);
 
