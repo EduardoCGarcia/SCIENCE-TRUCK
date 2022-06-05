0edc02fe4f273e0d05547a0d061b4291a693c1a4
EduardoCGarcia <echavezg004@gmail.com>
2022-06-04 20:38:30 -0500

    implementacion de guard para la pestaña de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckLoginGuard } from './guards/check-login.guard';
 
 
 const routes: Routes = [
@@ -7,7 +8,7 @@ const routes: Routes = [
   { path: 'divulgadores', loadChildren: () => import('./pages/divulgadores-page/divulgadores-page.module').then(m => m.DivulgadoresPageModule) },
   { path: 'cientificos', loadChildren: () => import('./pages/cientificos-page/cientificos-page.module').then(m => m.CientificosPageModule) },
   { path: 'productos', loadChildren: () => import('./pages/productos-page/productos-page.module').then(m => m.ProductosPageModule) },
-  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule), canActivate:[CheckLoginGuard]},
   { path: 'inicio', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path:'**',redirectTo:'inicio', pathMatch:'full'}
 ];
